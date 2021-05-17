5915d3c22eae50600e9e57f56e602c09e303fb70
pdomas <57319091+pdomas@users.noreply.github.com>
2021-05-17 09:46:49 +0300

    Add files via upload

---
+++ Bakalauro API/src/main/java/com/example/Bakalaurinis/BakalaurinisApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bakalaurinis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakalaurinisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakalaurinisApplication.class, args);
+	}
+
+}
---
+++ Bakalauro API/src/main/java/com/example/Bakalaurinis/entity/dto/LocationDTO.java
@@ -0,0 +1,77 @@
+package com.example.Bakalaurinis.entity.dto;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+public class LocationDTO {
+    private UUID location_id;
+
+    private String latitude;
+
+    private String longitude;
+
+    private String location_address;
+
+    private double price_per_hour;
+
+    private String contact_info;
+
+    private boolean reservation_status;
+
+    public UUID getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(UUID location_id) {
+        this.location_id = location_id;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLocation_address() {
+        return location_address;
+    }
+
+    public void setLocation_address(String location_address) {
+        this.location_address = location_address;
+    }
+
+    public String getContact_info() {
+        return contact_info;
+    }
+
+    public void setContact_info(String contact_info) {
+        this.contact_info = contact_info;
+    }
+
+    public boolean isReservation_status() {
+        return reservation_status;
+    }
+
+    public void setReservation_status(boolean reservation_status) {
+        this.reservation_status = reservation_status;
+    }
+
+    public double getPrice_per_hour() {
+        return price_per_hour;
+    }
+
+    public void setPrice_per_hour(double price_per_hour) {
+        this.price_per_hour = price_per_hour;
+    }
+}
